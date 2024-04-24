d50e9e63b8a3cc433bcb74a6e4999440a32d2332
fhhmm <takahashi.t66@gmail.com>
2024-04-24 15:23:39 +0900

    chore: Other: commit-msg

---
+++ commit-msg.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#####
+# .git/hooks/commit-msg
+#
+# ./commit-msg.sh $1
+# if [ $? != 0]; then
+#   exit 1
+# fi
+# exit 0
+#####
+
+COMMIT_MSG=`cat $1`
+
+# validate format
+FIELD_COUNT=`echo $COMMIT_MSG | awk -F ':' '{print NF}'`
+if [ $FIELD_COUNT -lt 3 ]; then
+  echo "\"$COMMIT_MSG\" is invalid format. Format: '{type}: {sub-type}: {message}'"
+  exit 1
+fi
+
+# validate type
+TYPE=`echo $COMMIT_MSG | awk -F ':' '{print $1}' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+case $TYPE in
+  "feat"|"chore"|"fix"|"test"|"docs"|"refactor")
+    ;;
+  *)
+    echo "\"$TYPE\" is invalid type."
+    exit 1
+esac
+
+# validate sub-type
+SUB_TYPE=`echo $COMMIT_MSG | awk -F ':' '{print $2}' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+case $SUB_TYPE in
+  "Scala"|"Python"|"Go"|"MySQL"|"AWS"|"GCP"|"Other")
+    ;;
+  *)
+    echo "\"$SUB_TYPE\" is invalid subtype."
+    exit 1
+esac
+
+exit 0
